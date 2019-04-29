1e1df4cffd44f06dfb4dbcd622b764cd1db20e63
Emma Pietsch <emmalyn.pietsch@outlook.com>
2019-04-29 14:42:12 +1000

    Edited views. Created sales, settings and suppliers views. Added AddressModel and Application Model.

---
+++ src/main/java/com/app/component/model/SubjectListCellModel.java
@@ -0,0 +1,15 @@
+package com.app.component.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class SubjectListCellModel {
+    private StringProperty subjectArea;
+
+    public SubjectListCellModel(String type) { subjectArea = new SimpleStringProperty(type); }
+
+    public String getSubjectArea() { return subjectArea.get(); }
+
+    void setSubjectArea(String type) { subjectArea.setValue(type);}
+    }
+
